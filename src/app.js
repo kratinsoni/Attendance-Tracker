9aa847113bfdc1d67f8c8f8148baef252f5d1941
Harshit Soni <kratin67soni@gmail.com>
2026-01-14 00:24:52 +0530

    Add Prettier configuration, update package.json to include dotenv, and modify app and index files for improved environment variable handling

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import 'dotenv/config';
+import cors from 'cors';
+
+// dotenv.config("./.env");
 
 const app = express();
 const PORT = process.env.PORT || 3000; 
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(cookieParser());
+
 
 export default app;
